124a236ea5d04ccfadb7934e8252a76d27717be5
Kunalgupta66 <126165374+Kunalgupta66@users.noreply.github.com>
2025-05-25 00:31:49 +0530

    Update Tip.py

--- Tip.py
+++ Tip.py
@@ -1,4 +1,4 @@
-print("Wwlcome to the tip calculater!")
+print("Wlcome to the tip calculater!")
 bill = int(input("What was the total bill? "))
 tip = int(input(f"How much tip would you like to give? {10} , {12}, or {15}?"))
 people = int(input("How much people to split the bill? "))
